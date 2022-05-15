2e6c1b0e9611c3b3744e4de83563c97a78c35a3c
Criss <rcristian1986@gmail.com>
2022-05-15 16:40:49 -0300

    commit jpa

--- src/main/java/com/portfolio/beportfolio/model/User.java
+++ src/main/java/com/portfolio/beportfolio/model/User.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,11 +18,11 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int idUser;
-    
     private String name;
     private String surname;
     private String email;
     private String phone;
+    private String street;
     private int number;
     private java.sql.Date birth;
     private int children;
@@ -31,16 +32,39 @@ public class User {
     private String mydescript;
     private String mydescript2;
     private String resume;
-    
+    @ManyToOne
+    private Civil idCivil;
+    @ManyToOne
+    private City idCity;
+    @ManyToOne
+    private Graduation idGraduation;
     
     public User(){
         
     }
     
-    public User(int idUser, String name, String surname){
+    public User(int idUser, String name, String surname, String email, String phone, 
+                String street, int number, java.sql.Date birth, int children, 
+                String picture,String iam, String aboutme, String mydescript, String mydescript2,
+                String resume, Civil idCivil, City idCity, Graduation idGraduation){
         this.idUser = idUser;
         this.name = name;
         this.surname = surname;
+        this.email = email;
+        this.phone = phone;
+        this.street = street;
+        this.number = number;
+        this.birth = birth;
+        this.children = children;
+        this.picture = picture;
+        this.iam = iam;
+        this.aboutme = aboutme;
+        this.mydescript = mydescript;
+        this.mydescript2 = mydescript2;
+        this.resume = resume;
+        this.idCivil = idCivil;
+        this.idCity = idCity;
+        this.idGraduation = idGraduation;
     }
     
 }
